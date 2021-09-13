9e313e3fb64521c62c1125a802ba1ebbd5aa75c4
Bruno Zevallos <bruno_zevallos@usmp.pe>
2021-09-13 15:13:11 -0500

    01 COMMIT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace repaso02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
